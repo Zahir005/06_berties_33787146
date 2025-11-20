6f36852c612ca936f4d2362d79e8763d8b6d750f
zahir005 <zahirmiah20019@gmail.com>
2025-11-20 11:07:18 +0000

    added some more testing

--- insert_test_data.sql
+++ insert_test_data.sql
@@ -1,5 +1,17 @@
-# Insert data into the tables
+-- Insert data into the tables
 
 USE berties_books;
 
-INSERT INTO books (name, price)VALUES('Brighton Rock', 20.25),('Brave New World', 25.00), ('Animal Farm', 12.99) ;
+-- Seed books table
+INSERT INTO books (name, price)
+VALUES
+  ('Brighton Rock', 20.25),
+  ('Brave New World', 25.00),
+  ('Animal Farm', 12.99);
+
+-- Seed default login user for marking:
+-- username: gold
+-- password: smiths
+INSERT INTO users (username, firstName, lastName, email, hashedPassword)
+VALUES
+  ('gold', 'Gold', 'User', 'gold@example.com', '$2b$10$dvmafyM293LjS1.Dh88Am.V0UZCRXcTfqh1i7vs0Xv/jakVYzt//a');
